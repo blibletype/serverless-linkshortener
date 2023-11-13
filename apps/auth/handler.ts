9511503e0148714523bc494449c49afd2488b467
blibletype <maxymkoval2510@gmail.com>
2023-11-13 13:16:50 +0200

    style: some adjustments

--- apps/auth/handler.ts
+++ apps/auth/handler.ts
@@ -2,6 +2,8 @@ import { AuthController } from "./controller/auth.controller";
 
 const authController = new AuthController();
 
-export const signUp = async (event: any) =>  authController.signUp(event);
+export const signUp = async (event: any) =>
+  authController.signUp(event);
 
-export const signIn = async (event: any) => authController.singIn(event);
+export const signIn = async (event: any) =>
+  authController.singIn(event);
