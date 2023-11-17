e835d90c269c3030fc348a554f12c8db28a6704a
blibletype <maxymkoval2510@gmail.com>
2023-11-17 10:44:10 +0200

    refactor: added try catch

--- verifyEmail.mjs
+++ verifyEmail.mjs
@@ -1,9 +1,13 @@
 import { SESClient, VerifyEmailIdentityCommand } from '@aws-sdk/client-ses';
 
 const run = async () => {
-  const email = process.env.SENDER_EMAIL_ADDRESS;
-  const ses = new SESClient({ region: process.env.REGION });
-  ses.send(new VerifyEmailIdentityCommand({ EmailAddress: email }));
+  try {
+    const email = process.env.SENDER_EMAIL_ADDRESS;
+    const ses = new SESClient({ region: process.env.REGION });
+    ses.send(new VerifyEmailIdentityCommand({ EmailAddress: email }));
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 run();
